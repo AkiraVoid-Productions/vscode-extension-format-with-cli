286a5ea4f2ae9c1cd794c15523fbb8c115a47ca6
AkiraVoid <me@akiravoid.com>
2024-02-06 23:28:28 +0800

    feat: support Direct Format (#4)
    Direct Format is a technology that uses native VS Code API to format document, rather than leave the formatting to formatter CLI. This change contributed an option:
    
    * `AkiraVoid.formatWithCLI.directFormat` - A `boolean` option, default to `true`.
    
    It also changed the default value of the option `AkiraVoid.formatWithCLI.command` to `"npx prettier --ignore-unknown \"{file}\""` because the Direct Format is now enabled by default, which makes the previous `--write` switch abundant.

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,21 @@
+import { execSync } from 'child_process';
+import { rmSync, writeFileSync } from 'fs';
+import path from 'path';
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('Format with CLI is now active!');
 
   vscode.languages.getLanguages().then(languages => {
-    const terminal =
-      vscode.window.terminals.find(t => t.name === 'Format with CLI') ??
-      vscode.window.createTerminal('Format with CLI');
     let disposable = vscode.languages.registerDocumentFormattingEditProvider(
       languages.map(language => ({ language })),
       {
         provideDocumentFormattingEdits(doc) {
-          try {
-            terminal.sendText(
-              getCommand(doc.languageId).replace('{file}', doc.fileName),
-            );
-            return [];
-          } catch (error) {
-            console.error(error);
+          if (!isDirectFormatEnabled()) {
+            return formatWithFormatter(doc);
           }
+
+          return formatDirectly(doc);
         },
       },
     );
@@ -33,4 +30,49 @@ const getCommand = (language?: string) =>
       'AkiraVoid.formatWithCLI',
       language ? { languageId: language } : undefined,
     )
-    .get<string>('command', 'npx prettier --write --ignore-unknown {file}');
+    .get<string>('command', 'npx prettier --ignore-unknown "{file}"');
+
+const isDirectFormatEnabled = (language?: string) =>
+  vscode.workspace
+    .getConfiguration(
+      'AkiraVoid.formatWithCLI',
+      language ? { languageId: language } : undefined,
+    )
+    .get<boolean>('directFormat', true);
+
+const formatWithFormatter = (doc: vscode.TextDocument) => {
+  const terminal =
+    vscode.window.terminals.find(t => t.name === 'Format with CLI') ??
+    vscode.window.createTerminal('Format with CLI');
+  terminal.hide();
+  try {
+    terminal.sendText(
+      getCommand(doc.languageId).replace('{file}', doc.fileName),
+    );
+    return [];
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const formatDirectly = (doc: vscode.TextDocument) => {
+  const content = doc.getText();
+  const cacheFile = path.join(
+    vscode.env.appRoot,
+    `.format-with-cli.cache${path.extname(doc.fileName)}`,
+  );
+  const command = getCommand(doc.languageId).replace('{file}', cacheFile);
+  try {
+    writeFileSync(cacheFile, content, { encoding: 'utf-8', flag: 'w' });
+    const output = execSync(command, { windowsHide: false, encoding: 'utf-8' });
+    rmSync(cacheFile);
+    const range = new vscode.Range(
+      doc.lineAt(0).range.start,
+      doc.lineAt(doc.lineCount - 1).range.end,
+    );
+    const edit = vscode.TextEdit.replace(range, output);
+    return [edit];
+  } catch (error) {
+    console.error(error);
+  }
+};
